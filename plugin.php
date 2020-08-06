0023adcae02867c0404c02ad71340cf9d9d0cae4
Tobias Goltz <tobias.goltz@integral-learning.com>
2020-08-06 11:19:39 +0200

    19565 updated version

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = "xmum";
 
 // code version; must be changed for all code changes
-$version = "1.1.0";
+$version = "1.1.1";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
