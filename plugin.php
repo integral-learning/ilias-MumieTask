6458cbd24bc22557db295439854501e48492516b
Tobias Goltz <tobias.goltz@integral-learning.com>
2021-04-28 16:26:07 +0200

    updated max supported ilias version

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@ $version = "1.1.2";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.4.4";
-$ilias_max_version = "5.4.999";
+$ilias_max_version = "6.8";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Tobias Goltz";
