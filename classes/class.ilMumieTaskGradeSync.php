e16975df27f139aee6f7fc568a8a99117b064975
Vasovasovasovaso <vasilije.nedeljkovic@integral-learning.de>
2022-10-04 12:31:52 +0200

    impented round

--- classes/class.ilMumieTaskGradeSync.php
+++ classes/class.ilMumieTaskGradeSync.php
@@ -87,7 +87,6 @@ class ilMumieTaskGradeSync
             )
         );
         $response = json_decode($curl->exec());
-        ilLoggerFactory::getLogger('xmum')->info(json_encode($response));
         $curl->close();
         return($response);
     }
