8c3c946c47ccb37511d5a8181e69283736e744e9
Vasovasovasovaso <vasilije.nedeljkovic@integral-learning.de>
2022-10-05 11:46:42 +0200

    added grade update to user list

--- classes/class.ilMumieTaskUserListGUI.php
+++ classes/class.ilMumieTaskUserListGUI.php
@@ -39,7 +39,7 @@ class ilMumieTaskUserListGUI extends ilTable2GUI
             $members = $this->getAllIds();
         }
 
-
+        ilMumieTaskLPStatus::updateGrades($parentObj->object);
 
         $this->tpl->addBlockFile(
             "TBL_CONTENT",
