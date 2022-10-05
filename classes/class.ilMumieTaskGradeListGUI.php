df0d736a9dd88cc1113bc17da8fcb598699b7c05
Vasovasovasovaso <vasilije.nedeljkovic@integral-learning.de>
2022-10-05 09:48:44 +0200

    grade id added to table

--- classes/class.ilMumieTaskGradeListGUI.php
+++ classes/class.ilMumieTaskGradeListGUI.php
@@ -60,7 +60,7 @@ class ilMumieTaskGradeListGUI extends ilTable2GUI
                         : "tblrow2";
 
                     $this->tpl->setVariable("CSS_ROW", $this->css_row);
-                    $this->tpl->setVariable("VAL_GRADE", round($xGrade->result->score->raw * 100));
+                    $this->tpl->setVariable("VAL_GRADE", round($xGrade->result->score->raw * 100) . " " . $xGrade->id);
                     $this->ctrl->setParameterByClass('ilObjMumieTaskGUI', 'member_id', $user_id);
                     $this->ctrl->setParameterByClass('ilObjMumieTaskGUI', 'updateGradeId', $xGrade->id);
 
