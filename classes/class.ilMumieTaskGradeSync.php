604f5927ce8b3f6e5a9b905a8e925bfa21e7b6d7
Vasovasovasovaso <vasilije.nedeljkovic@integral-learning.de>
2022-10-05 09:42:30 +0200

    still json logger

--- classes/class.ilMumieTaskGradeSync.php
+++ classes/class.ilMumieTaskGradeSync.php
@@ -87,7 +87,7 @@ class ilMumieTaskGradeSync
             )
         );
         $response = json_decode($curl->exec());
-        ilLoggerFactory::getLogger('xmum')->info(print_r(json_encode($response), true));
+        ilLoggerFactory::getLogger('xmum')->info(json_encode($response));
         $curl->close();
         return($response);
     }
