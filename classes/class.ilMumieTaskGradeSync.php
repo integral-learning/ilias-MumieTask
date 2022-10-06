b36ca23e22743bbb972e187a89384127298839a5
Vasovasovasovaso <vasilije.nedeljkovic@integral-learning.de>
2022-10-06 08:46:15 +0200

    logger changed

--- classes/class.ilMumieTaskGradeSync.php
+++ classes/class.ilMumieTaskGradeSync.php
@@ -87,7 +87,7 @@ class ilMumieTaskGradeSync
             )
         );
         $response = json_decode($curl->exec());
-        ilLoggerFactory::getLogger('xmum')->info("response :" . json_encode($response));
+        ilLoggerFactory::getLogger('xmum')->info("response :" . print_r(($response)));
         $curl->close();
         return($response);
     }
