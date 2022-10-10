6233f2701304cf8add8045c4467360b4b1893eeb
Vasovasovasovaso <vasilije.nedeljkovic@integral-learning.de>
2022-10-10 10:25:03 +0200

    added logger

--- classes/class.ilMumieTaskGradeSync.php
+++ classes/class.ilMumieTaskGradeSync.php
@@ -182,10 +182,11 @@ class ilMumieTaskGradeSync
                 $xapi_grades = array_filter($xapi_grades, array($this, "isGradeBeforeDueDate"));
                 $valid_grade_by_user[$user_id] = $this->getLatestGrade($xapi_grades);
             } else {
+                ilLoggerFactory::getLogger('xmum')->info("overriden grade ". print_r($$this->wasGradeOverriden($user_id, $xapi_grades), true));
                 $valid_grade_by_user[$user_id] = $this->wasGradeOverriden($user_id, $xapi_grades);
             }
         }
-        ilLoggerFactory::getLogger('xmum')->info("valid grades ". print_r($valid_grade_by_user, true));
+        ilLoggerFactory::getLogger('xmum')->info("valid grades before filter ". print_r($valid_grade_by_user, true));
         return array_filter($valid_grade_by_user);
     }
 
