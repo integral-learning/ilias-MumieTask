d3e11503f999f9102f789eefb6f74653b2eec073
tobias <tobias.goltz@integral-learning.de>
2023-01-16 10:05:35 +0100

    Pass user_id as param in constructor of DueDateExtensionForm instead of reading it from params every time we use it

--- classes/class.ilObjMumieTaskGUI.php
+++ classes/class.ilObjMumieTaskGUI.php
@@ -697,7 +697,7 @@ class ilObjMumieTaskGUI extends ilObjectPluginGUI
     {
         global $ilCtrl, $lng;
         require_once('./Customizing/global/plugins/Services/Repository/RepositoryObject/MumieTask/classes/forms/class.ilMumieTaskDueDateExtensionForm.php');
-        $form = new ilMumieTaskDueDateExtensionForm($this->object);
+        $form = new ilMumieTaskDueDateExtensionForm($this->object, $_GET["user_id"]);
         $form->setTitle($lng->txt('rep_robj_xmum_frm_user_overview_list_deadline_extension'));
         $form->checkInput();
         $form->setFields();
--- classes/forms/class.ilMumieTaskDueDateExtensionForm.php
+++ classes/forms/class.ilMumieTaskDueDateExtensionForm.php
@@ -14,22 +14,27 @@ class ilMumieTaskDueDateExtensionForm extends ilPropertyFormGUI
 {
     private $date_input;
     private $mumie_task;
+    /**
+     * @var string
+     */
+    private $user_id;
 
-    public function __construct($mumie_task)
+    public function __construct($mumie_task, $user_id)
     {
         parent::__construct();
         $this->mumie_task = $mumie_task;
+        $this->user_id = $user_id;
     }
 
     public function setFields()
     {
         global $lng;
-        $this->ctrl->setParameterByClass('ilObjMumieTaskGUI', 'user_id', $_GET["user_id"]);
+        $this->ctrl->setParameterByClass('ilObjMumieTaskGUI', 'user_id', $this->user_id);
         $this->date_input = new ilDateTimeInputGUI($lng->txt('rep_robj_xmum_frm_deadline_extension_new_deadline'), 'dateTime');
         $this->date_input->setShowTime(true);
         require_once('./Customizing/global/plugins/Services/Repository/RepositoryObject/MumieTask/classes/DeadlineExtension/class.ilMumieDeadlineExtensionService.php');
-        if(ilMumieDeadlineExtensionService::hasDeadlineExtension($_GET["user_id"], $this->mumie_task)){
-            $date_time = ilMumieDeadlineExtensionService::getDeadlineExtensionDate($_GET["user_id"], $this->mumie_task);
+        if(ilMumieDeadlineExtensionService::hasDeadlineExtension($this->user_id, $this->mumie_task)){
+            $date_time = ilMumieDeadlineExtensionService::getDeadlineExtensionDate($this->user_id, $this->mumie_task);
         } else {
             $date_time = new ilDateTime($this->mumie_task->getActivationEndingTime(), IL_CAL_UNIX);
         }
