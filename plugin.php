c3051c13e86e49ee973abadf37b1ce4a2a146aba
tobias <tobias.goltz@integral-learning.de>
2023-04-03 17:37:17 +0200

    Updated version and changelog

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 $id = "xmum";
 
 // code version; must be changed for all code changes
-$version = "2.3";
+$version = "2.4";
 
 
 // ilias min and max version; must always reflect the versions that should
