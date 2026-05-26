f73e1e0c1233313538246477b4ff8d6cc07f1003
Dung Pham <dung.pham@integral-learning.de>
2026-05-26 18:36:02 +0200

    fix paths for verifying token

--- plugin.php
+++ plugin.php
@@ -3,11 +3,11 @@
 $id = 'xmum';
 
 // code version; must be changed for all code changes
-$version = '4.0.1';
+$version = '4.0.3';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '9.0';
+$ilias_min_version = '10.0';
 $ilias_max_version = '10.99';
 
 // optional, but useful: Add one or more responsible persons and a contact email
