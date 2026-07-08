fe4589b3a1d1f5bf08a47353c71734926d5ee0fb
greiser <sabine.greiser@integral-learning.de>
2026-07-08 13:29:24 +0200

    Add public key endpoint for deadline signature verification (Phase 4), bump to 7.0.1

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 $id = 'xmum';
 
 // code version; must be changed for all code changes
-$version = '7.0';
+$version = '7.0.1';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
